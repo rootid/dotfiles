c3179c295d019084c6ac43f9ba4a29c0b4cee34c
rootid <vsinhsawant@gmail.com>
2023-08-26 08:34:24 -0400

    WIP

--- tools/utils/go_get_week_number.py
+++ tools/utils/go_get_week_number.py
@@ -2,11 +2,22 @@
 
 import datetime
 
+LOGGED_WEEK_NUMBER  = 27
 # Get the current date
-current_date = datetime.date.today()
+today  = datetime.date.today()
 
 # Get the week number
-week_number = current_date.isocalendar()[1]
+week_number = today.isocalendar()[1]
+month_name = today.strftime("%b").lower()
+year = today.strftime("%y").lower()
+# My week starts from Sunday
+if (datetime.date.today().isoweekday() == 7) :
+  week_number += 1
 
+sprint_no = week_number - LOGGED_WEEK_NUMBER
 # Print the week number
-print(f"week number is {week_number} and data is {current_date}")
+print(f"week number is {week_number} and data is {today}")
+# mmm_week#_sprint#_year e.g. aug_32_5_23
+# e.g. create planner  touch ~/plain_docs/planner/gtd/aug_32_5_23.org
+# Update the
+print(f"{month_name}_{week_number}_{sprint_no}_{year}")

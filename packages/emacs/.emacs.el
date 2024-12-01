f14474a40cd269289601827dd9db7746c8857521
rootid <vsinhsawant@gmail.com>
2024-12-01 17:01:59 -0500

    Unify elisp

--- packages/emacs/.emacs.el
+++ packages/emacs/.emacs.el
@@ -1,5 +1,4 @@
-
-;; Initialize package sources
+;; Initializ package sources
 (require 'package)
 
 ;; package-archives keeps track of all existing sources of packages 
@@ -10,8 +9,10 @@
 
 ;; No automatic package loading of packages 
 (setq package-enable-at-startup nil)
+
 ;; Initialize the package system and load installed packages. 
 (package-initialize) 
+
 ;; make sure package archive contents are updated
 (unless package-archive-contents (package-refresh-contents))
 
@@ -20,29 +21,28 @@
 (package-refresh-contents)
 (package-install 'use-package))
 
+;; TODO - Additional config https://protesilaos.com/emacs/modus-themes#h:f0f3dbcb-602d-40cf-b918-8f929c441baf
+(add-to-list 'load-path "~/dotfiles/packages/emacs/emacs_themes/modus-themes")
+(load-theme 'modus-operandi)            ; Light theme
+
+;; TODO - Explore straight.el vs packge.el
+;; Use of straight.el 
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name
+        "straight/repos/straight.el/bootstrap.el"
+        (or (bound-and-true-p straight-base-dir)
+            user-emacs-directory)))
+      (bootstrap-version 7))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
 ;; org mode is installed already if you use Emacs version >= 24
 (require 'org)
-
-;;(use-package org-babel
-;;  :ensure t
-;;  :config
-;;  (org-babel-mode))
-;;(org-babel-mode)
-;;(add-hook 'org-mode-hook 'org-babel-mode)
-;;(setq org-babel-default-language "emacs-lisp")
-;;(setq org-babel-load-languages '((emacs-lisp . t) (python . t)))
-
 (org-babel-load-file (expand-file-name "~/emacs_config/basic.org"))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(org-download org-babel which-key use-package rainbow-delimiters ivy-rich iodine-theme helpful general evil elisp-format counsel command-log-mode)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(org-habit ((t (:foreground "green")))))
--- tools/emacs/aliases.sh
+++ tools/emacs/aliases.sh
@@ -1,5 +1,9 @@
-alias emacs_vanilla='emacs --no-init-file --no-loadup'
-alias emacs_custom='emacs --no-splash --no-init-file --load'
+alias evi='emacs --no-splash'
 
+alias emacs_vanilla='emacs --no-init-file --no-loadup'
+
+# Use with .el script and use like this for init.el script
+# emacs_custom init.el
+alias emacs_custom='emacs --no-splash --no-init-file --load'
 # TODO - setup experimental version, check how to use chdir
 alias emacs_exp='emacs --no-init-file '
